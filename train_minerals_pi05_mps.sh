9c0a08854c0c355e493c5e0ed210bb6988c0c0f5
Giovanni Marchetti <gimarchetti@google.com>
2026-01-07 12:08:33 -0800

    update minerals training

--- train_minerals_pi05_mps.sh
+++ train_minerals_pi05_mps.sh
@@ -1,13 +1,13 @@
 PYTORCH_ENABLE_MPS_FALLBACK=1 lerobot-train \
-	--dataset.repo_id="gimarchetti/so101-winnie-us7" \
+	--dataset.repo_id="gimarchetti/sd101-minerals-leo-3" \
 	--policy.type="pi05" \
 	--batch_size=4 \
 	--steps=30000 \
-	--output_dir="outputs/train/pi05_so101_winnie_us7_mps" \
-	--job_name=pi05_so101_winnie_us7_mps_bf16 \
+	--output_dir="outputs/train/pi05_sd101_minerals_leo_3" \
+	--job_name=pi05_sd101_minerals_leo_3_mps_bf16 \
 	--policy.device=mps \
 	--wandb.enable=true \
-	--policy.repo_id="gimarchetti/winnie_us7_pi05_policy_mps_bf16" \
+	--policy.repo_id="gimarchetti/pi05_so101_policy_mps_bf16" \
 	--policy.compile_model=false \
 	--policy.gradient_checkpointing=true \
 	--policy.pretrained_path=lerobot/pi05_base \
